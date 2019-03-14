19bfba94f3fdaf9e0362be218ea08e29ab51cc55
CreeJee <000as000@naver.com>
2019-03-15 00:32:57 +0900

    delegated event resolve

--- Component/core/event.js
+++ Component/core/event.js
@@ -1,11 +1,14 @@
 
 import Store from "./store.js";
 import VSElement from "./vs-element.js";
-const __dispatchNearest = (selectedNode,key,value)=>{
-    let __recursive = (node)=> (node instanceof VSElement) ? node.$store.dispatch(key,value) : Array.from(node.children).forEach(__recursive);
+const __travelNode = (selectedNode,findCallback)=>{
+    let __recursive = (node)=> (node instanceof VSElement) ? findCallback(node) : Array.from(node.children).forEach(__recursive);
     return __recursive(selectedNode);
 }
-const _inserthNode = (appendHost,isInsert,key,value,node,k)=>{
+const __dispatchNearest = (selectedNode,key,value)=>{
+    return __travelNode(selectedNode,(node)=>node.dispatch(key,value));
+}
+const __inserthNode = (appendHost,isInsert,key,value,node,k)=>{
     
     let selectedNode =  isInsert ? appendHost.appendChild(node.cloneNode(true)) : appendHost.children[k];
     __dispatchNearest(selectedNode,key,value);
@@ -14,7 +17,7 @@ export default {
     dispatchAppend : (assignedElements,appendHost,k,v,isInsert = true)=>{
         let childStore = appendHost.$store.children;
         if(Array.isArray(assignedElements)){
-            assignedElements.forEach(_inserthNode.bind(null,appendHost,isInsert,k,v));
+            assignedElements.forEach(__inserthNode.bind(null,appendHost,isInsert,k,v));
         }
         else{
             throw new Error("unknown dispatch target");
@@ -30,5 +33,6 @@ export default {
         catch(e){
             throw new Error(`undefined expression on [${expr}]`);
         }
-    }
+    },
+    travelNode : __travelNode,
 };
--- Component/core/vs-element.js
+++ Component/core/vs-element.js
@@ -1,4 +1,5 @@
 import Store from "./store.js";
+import VSEvent from "./event.js";
 const $root = Store.root;
 function VSElement(superConstructor = HTMLElement){
     if(!(Object.getPrototypeOf(superConstructor) === HTMLElement || superConstructor === HTMLElement)){
@@ -20,6 +21,27 @@ function VSElement(superConstructor = HTMLElement){
         async VShadow(...args){
             return Promise.reject(new Error(`need implements [async ${this.name}.VShadow()]`));
         }
+        async promiseEvent(selector,type){
+            return new Promise((resolve,reject)=>{
+                let result = Array.from(this.querySelectorAll(selector));
+                if(result.length === 0){
+                    reject();
+                }
+                result.forEach((node)=>node.addEventListener(type,function(e){
+                    resolve(e);
+                }))
+            })
+        }
+        async delegatedPromiseEvent(selector,type){
+            return new Promise((resolve,reject)=>{
+                this.addEventListener(type,(e)=>{
+                    if(Array.from(this.querySelectorAll(selector)).includes(e.target)){
+                        resolve(e);
+                    }
+                })
+            })
+        }
+
     }
 };
 export default VSElement();
