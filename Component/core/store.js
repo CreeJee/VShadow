56a1fa104bc0aaf3c4d8d369ae7e5e5e0cde7791
CreeJee <000as000@naver.com>
2019-03-29 16:36:51 +0900

    proxy change

--- Component/core/store.js
+++ Component/core/store.js
@@ -11,7 +11,10 @@ let _Store = null;
 let wrapProxy = (o)=>{
     return new Proxy(o,{
         set : (obj,prop,value)=>(obj.dispatch(prop,value,obj),true),
-        get : (obj,prop)=>prop in o ? o[prop] instanceof Function ? o[prop].bind(this) : o[prop] : obj.get(prop)
+        get : (obj,prop)=>{
+            let v = Reflect.get(o,prop);
+            return prop in o ? v instanceof Function ? v.bind(this) : v : obj.get(prop)
+        }
     })
 }
 export default class Store extends Map{
