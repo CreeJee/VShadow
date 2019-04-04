78311a04d74da42b164da40ff8f636456bcd9ac8
CreeJee <000as000@naver.com>
2019-04-04 17:59:35 +0900

    vs-loop slot problem fix

--- Component/vs-loop.js
+++ Component/vs-loop.js
@@ -84,8 +84,7 @@ const VSLoopGen = (superClass) => class VSLoop extends VSElement.extend(superCla
     }
     async VShadow(root,$store){
         const attributes = this.attributes;
-        const slot = root.querySelector("#slot");
-        const assignedElements = slot.assignedElements();
+        const assignedElements = (this[VSElement.notShadowSymbol]) ? Array.from(this.children) : root.querySelector("#slot").assignedElements();
         let temp = -1;
         let iterateStart = null,iterateTotal = null;
         let iterateAsArray = [];
